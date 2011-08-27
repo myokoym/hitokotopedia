28f33fca36e6c3f56f499254faff841542fe44c8
myokoym <myokoym@gmail.com>
2011-08-28 02:35:35 +0900

    不要なmigrationを削除

--- db/migrate/20110827132143_description_type_to_text.rb
+++
@@ -1,9 +0,0 @@
-class DescriptionTypeToText < ActiveRecord::Migration
-  def self.up
-    add_column :describes, :describe, :text
-  end
-
-  def self.down
-    remove_column :describes, :description
-  end
-end
