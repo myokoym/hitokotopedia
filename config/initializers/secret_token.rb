b6a9d1cb66e6ee4b7bb66249688c7b496d0e7c2d
myokoym <myokoym@gmail.com>
2011-08-27 19:19:27 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hitokotode::Application.config.secret_token = 'cf5b95d570116667d615742c5da79ec185000376041042dd7f149a415365e99251c510510342f44e0fcc649da5439d0ffccb804d57b8643b429b61f84a40c4c6'
